d297c66ec0fa28319ac82f00acbc52474c4bf54d
Michael Galambos <michael.galambos@agilita.ch>
2023-09-29 11:23:30 +0200

    scrolling loads months correctly and deletes old observers

--- webapp/controller/Week.controller.js
+++ webapp/controller/Week.controller.js
@@ -33,25 +33,25 @@ sap.ui.define(
           for (let i = 1; i <= lastday; i++) {
             date
               .getData()
-              .push(
+              .push([
                 new Date(
                   month.getFullYear(),
                   month.getMonth(),
                   i
-                ).toLocaleDateString()
-              );
+                ).toLocaleDateString(),
+              ]);
           }
         } else {
           for (let i = lastday; i > 0; i--) {
             date
               .getData()
-              .unshift(
+              .unshift([
                 new Date(
                   month.getFullYear(),
                   month.getMonth(),
                   i
-                ).toLocaleDateString()
-              );
+                ).toLocaleDateString(),
+              ]);
           }
         }
 
@@ -68,47 +68,50 @@ sap.ui.define(
       onPressRout: function () {
         this.getOwnerComponent().getRouter().navTo("Overview");
       },
-      nextMonth: function () {
-        let date = this.getView().getModel("date");
-        let month = new Date(date.getData()[date.getData().length - 1]);
-        month = new Date(month.getFullYear(), month.getMonth() + 1);
-        date = this.loadMonth(date, month, true);
-        this.getView().getModel("date").setSizeLimit(date.getData().length);
-        this.getView().getModel("date").refresh();
-        console.log(date)
-      },
-      prevMonth: function () {
-        let date = this.getView().getModel("date");
-        let month = new Date(date.getData()[0]);
-        month = new Date(month.getFullYear(), month.getMonth() - 1);
-        date = this.loadMonth(date, month, false);
-        this.getView().getModel("date").setSizeLimit(date.getData().length);
-        this.getView().getModel("date").refresh();
-        this.byId("idScrollContainer").scrollTo(0, 1500);
-
-      },
       observer: function () {
         let first, last;
-        this.allObserver = new IntersectionObserver((entries) => {
-          const items = this.getView().byId("scrollGrid").getItems();
-          if (entries[0].isIntersecting) {
-            if (entries[0].target === items[0].getDomRef()) {
-              this.prevMonth()
-            } else {
-              this.nextMonth()
-            }
-            entries.forEach(element => {
-              this.allObserver.unobserve(element.target)
-            })
-            this.allObserver.observe(items[0].getDomRef());
-            this.allObserver.observe(items[items.length - 1].getDomRef());
-          }
-        }, {
-          root: this.getView().byId("idScrollContainer").getDomRef(),
-          threshold: 0.9
-        });
-      },
+        this.allObserver = new IntersectionObserver(
+          (entries) => {
+            if (entries[0].isIntersecting) {
+              let items = this.getView().byId("scrollGrid").getItems();
+              let date = this.getView().getModel("date");
 
+              if (entries[0].target === items[0].getDomRef()) {
+                let month = new Date(date.getData()[0]);
+                month = new Date(month.getFullYear(), month.getMonth() - 1);
+                date = this.loadMonth(date, month, false);
+                this.getView()
+                  .getModel("date")
+                  .setSizeLimit(date.getData().length);
+                this.getView().getModel("date").refresh();
+                this.byId("idScrollContainer").scrollTo(0, 1500);
+              } else {
+                let month = new Date(date.getData()[date.getData().length - 1]);
+                month = new Date(month.getFullYear(), month.getMonth() + 1);
+                date = this.loadMonth(date, month, true);
+                this.getView()
+                  .getModel("date")
+                  .setSizeLimit(date.getData().length);
+                this.getView().getModel("date").refresh();
+              }
+              this.allObserver.unobserve(items[0].getDomRef());
+              this.allObserver.unobserve(items[items.length - 1].getDomRef());
+              items = this.getView().byId("scrollGrid").getItems();
+              this.delay(1).then(() => {
+                this.allObserver.observe(items[0].getDomRef());
+                this.allObserver.observe(items[items.length - 1].getDomRef());
+              });
+            }
+          },
+          {
+            root: this.getView().byId("idScrollContainer").getDomRef(),
+            rootMargin: "200px",
+          }
+        );
+      },
+      delay: function (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+      },
     });
   }
 );
